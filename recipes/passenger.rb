9d673cc6aaf8869cae83e6b9e8f29985f2443fcc
Josh Yotty <jyotty@bluebox.net>
2013-02-11 12:29:26 -0800

    add passenger_gemfile attribute

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -54,6 +54,7 @@ gem_package 'passenger' do
   action :install
   version node["nginx"]["passenger"]["version"]
   gem_binary node["nginx"]["passenger"]["gem_binary"] if node["nginx"]["passenger"]["gem_binary"]
+  source node["nginx"]["passenger"]["passenger_gemfile"] if node["nginx"]["passenger"]["passenger_gemfile"]
 end
 
 template "#{node["nginx"]["dir"]}/conf.d/passenger.conf" do
